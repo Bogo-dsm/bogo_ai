3f9568dd6ae57627cc73df48fdfceb01a3fd91d6
ZXO1114 <jio45298114@gmail.com>
2026-03-09 22:29:22 +0900

    feat: 코드 함수화 #3

--- package/summarizer/loader.py
+++ package/summarizer/loader.py
@@ -1,23 +1,26 @@
 from langchain_community.document_loaders import PDFPlumberLoader
 
-loader = PDFPlumberLoader("testpdf.pdf")
-docs = loader.load()
+def pdf_load(pdf_path : str) -> str:
+    loader = PDFPlumberLoader(pdf_path)
+    docs = loader.load()
 
-text = str()
-for doc in docs:
-    text += doc.page_content
+    text = str()
+    for doc in docs:
+        text += doc.page_content
 
-import re
+    import re
 
-roman_numerals = {'Ⅰ':'1', 'Ⅱ':'2', 'Ⅲ':'3', 'Ⅳ':'4', 'Ⅴ':'5'}
+    roman_numerals = {'Ⅰ': '1', 'Ⅱ': '2', 'Ⅲ': '3', 'Ⅳ': '4', 'Ⅴ': '5'}
 
-text = text.replace('\n\n','[[NEWLINE]]')
-text = re.sub(r'\n\s*-\s*\d+\s*-\s*\n', '\n', text)
-text = re.sub(r'(?<![.!?])\n', ' ', text)
-text = text.replace('[[NEWLINE]]','\n\n')
-text = re.sub(r' +', ' ', text)
-for roman_numeral in roman_numerals:
-    text = text.replace(roman_numeral,roman_numerals[roman_numeral])
-re_text = text.strip()
+    text = text.replace('\n\n', '[[NEWLINE]]')
+    text = re.sub(r'\n\s*-\s*\d+\s*-\s*\n', '\n', text)
+    text = re.sub(r'(?<![.!?])\n', ' ', text)
+    text = text.replace('[[NEWLINE]]', '\n\n')
+    text = re.sub(r' +', ' ', text)
+    for roman_numeral in roman_numerals:
+        text = text.replace(roman_numeral, roman_numerals[roman_numeral])
+    re_text = text.strip()
 
-print(re_text)
+    return re_text
+
+print(pdf_load('testpdf.pdf'))

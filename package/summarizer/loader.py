a1a647483607b6a3ddc6537f65a6def6523fe9a5
ZXO1114 <jio45298114@gmail.com>
2026-03-09 20:00:56 +0900

    feat: 청킹 제거, 텍스트 정규화 #3

--- package/summarizer/loader.py
+++ package/summarizer/loader.py
@@ -1,13 +1,7 @@
 from langchain_community.document_loaders import PDFPlumberLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 loader = PDFPlumberLoader("testpdf.pdf")
 docs = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 300,
-    chunk_overlap = 0
-)
 
 text = str()
 for doc in docs:
@@ -15,16 +9,15 @@ for doc in docs:
 
 import re
 
+roman_numerals = {'Ⅰ':'1', 'Ⅱ':'2', 'Ⅲ':'3', 'Ⅳ':'4', 'Ⅴ':'5'}
+
 text = text.replace('\n\n','[[NEWLINE]]')
 text = re.sub(r'\n\s*-\s*\d+\s*-\s*\n', '\n', text)
 text = re.sub(r'(?<![.!?])\n', ' ', text)
 text = text.replace('[[NEWLINE]]','\n\n')
 text = re.sub(r' +', ' ', text)
-text = text.strip()
+for roman_numeral in roman_numerals:
+    text = text.replace(roman_numeral,roman_numerals[roman_numeral])
+re_text = text.strip()
 
-recursive_text = text_splitter.split_text(text)
-
-for i,chunk in enumerate(recursive_text):
-    print(f"-----{i+1}번째 chunk-----")
-    print(chunk, end='\n\n')
-
+print(re_text)

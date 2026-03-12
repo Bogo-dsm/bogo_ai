f950c2324eac3af4018ff5241b3cd9651bf67807
ZXO1114 <jio45298114@gmail.com>
2026-03-12 15:47:17 +0900

    feat: json_schema.required and descriptions fix #4

--- package/summarizer/schemas.py
+++ package/summarizer/schemas.py
@@ -1,19 +1,20 @@
 json_schema = {
-    "title": "PDF 요약",
-    "description": "PDF를 텍스트로 변환한 글에 대한 요약",
+    "title": "PDF_Summary",
+    "description": "보고서 양식 전이를 위한 보고서 요약",
     "type": "object",
     "properties": {
         "tone": {
             "type": "string",
-            "description": "사용자를 위한 PDF 한 줄 요약"
+            "description": "사용자를 위한 보고서 특징 한 줄 요약"
         },
-        "description": {
+        "desc": {
             "type": "string",
-            "description": "사용자를 위한 PDF 상세 요약"
+            "description": "사용자를 위한 보고서 특징 상세 요약"
         },
         "character": {
             "type": "string",
-            "description": "AI 프롬프팅을 위한 PDF 상세요약"
+            "description": "양식 전이를 위한 AI 프롬프팅용 보고서 특징 상세요약"
         }
-    }
+    },
+    "required": ["tone", "desc", "character"]
 }
